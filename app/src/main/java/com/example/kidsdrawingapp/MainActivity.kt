f73753dab7dbc45c64ec9819a73c53d096c9066f
Halil Olcay <holcay92@gmail.com>
2023-03-12 00:53:02 +0300

    storage permission dialog imğlemented

--- app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
@@ -1,24 +1,47 @@
 package com.example.kidsdrawingapp
 
 
+import android.Manifest
 import android.os.Bundle;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import android.app.Dialog
+import android.os.Build
 import android.os.CountDownTimer
 import android.widget.ImageButton
 import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
-import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
     private var drawingView: DrawingView? = null
 
     private var mImageButtonCurrentPaint: ImageButton? = null
+    private val storagePermission: ActivityResultLauncher<Array<String>> =
+        registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            permissions.entries.forEach {
+                val permission = it.key
+                val isGranted = it.value
+                if (isGranted) {
+                    if (permission == Manifest.permission.READ_EXTERNAL_STORAGE) {
+                        Toast.makeText(this, "Storage permission granted", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+
+                } else {
+                    if (permission == Manifest.permission.READ_EXTERNAL_STORAGE) {
+                        Toast.makeText(this, "Storage permission denied", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,6 +59,10 @@ class MainActivity : AppCompatActivity() {
         val ibBrush = findViewById<ImageButton>(R.id.ib_brush)
         ibBrush.setOnClickListener {
             showBrushSizeChooserDialog()
+        }
+        val ibGallery = findViewById<ImageButton>(R.id.ib_gallery)
+        ibGallery.setOnClickListener {
+           requestStoragePermission()
         }
     }
 
@@ -62,7 +89,7 @@ class MainActivity : AppCompatActivity() {
         mediumBtn.setOnClickListener {
             drawingView?.setSizeForBrush(20.toFloat())
             brushDialog.dismiss()
-           customProgressDialogFunction()
+            customProgressDialogFunction()
             Toast.makeText(this, "Brush size: 20", Toast.LENGTH_SHORT).show()
         }
         val largeBtn = brushDialog.findViewById(R.id.ll_large_brush) as LinearLayout
@@ -70,14 +97,14 @@ class MainActivity : AppCompatActivity() {
             drawingView?.setSizeForBrush(30.toFloat())
             brushDialog.dismiss()
             customProgressDialogFunction()
-           // Snackbar.make(findViewById(R.id.drawing_view), "Brush size: 30", Snackbar.LENGTH_SHORT).show()
+            // Snackbar.make(findViewById(R.id.drawing_view), "Brush size: 30", Snackbar.LENGTH_SHORT).show()
             Toast.makeText(this, "Brush size: 30", Toast.LENGTH_SHORT).show()
         }
 
         brushDialog.show()
     }
 
-     fun paintClicked(view: View) {
+    fun paintClicked(view: View) {
         if (view !== mImageButtonCurrentPaint) {
 
             // type cast the view to ImageButton because it can be any view
@@ -124,12 +151,12 @@ class MainActivity : AppCompatActivity() {
 
     private fun customDialogFunction() {
         val customDialog = Dialog(this)
-       // customDialog.setContentView(R.layout.custom_dialog)
         // customDialog.setContentView(R.layout.custom_dialog)
-       /* customDialog.tv_submit.setOnClickListener {
-            Toast.makeText(this, "Clicked Submit", Toast.LENGTH_SHORT).show()
-            customDialog.dismiss()
-        }*/
+        // customDialog.setContentView(R.layout.custom_dialog)
+        /* customDialog.tv_submit.setOnClickListener {
+             Toast.makeText(this, "Clicked Submit", Toast.LENGTH_SHORT).show()
+             customDialog.dismiss()
+         }*/
         /* customDialog.tv_cancel.setOnClickListener {
            Toast.makeText(this, "Clicked Cancel", Toast.LENGTH_SHORT).show()
            customDialog.dismiss()
@@ -145,11 +172,41 @@ class MainActivity : AppCompatActivity() {
             override fun onTick(millisUntilFinished: Long) {
                 // do nothing
             }
+
             override fun onFinish() {
                 customProgressDialog.dismiss()
             }
         }
-           timer.start()
-   }
+        timer.start()
+    }
+
+    private fun showRationaleDialog(
+        title: String,
+        message: String,
+    ) {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle(title)
+            .setMessage(message)
+            .setPositiveButton("OK") { dialog, _ ->
+                // Request the permission
+                dialog.dismiss()
+            }
+        builder.create().show()
+    }
+    private fun requestStoragePermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && shouldShowRequestPermissionRationale(
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            )
+        ) {
+            showRationaleDialog(
+                "Storage Permission",
+                "This app needs storage permission to access your gallery."
+            )
+        } else {
+            // this is how you request a permission
+            storagePermission.launch(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE))
+            // TODO - add writing external storage permission
+        }
+    }
 
 }

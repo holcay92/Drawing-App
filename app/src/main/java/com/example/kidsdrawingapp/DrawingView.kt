679223266c52bdf250b32a166adf308df753feca
Halil Olcay <holcay92@gmail.com>
2023-03-11 15:25:52 +0300

    drawing screen finished. drawings stay premanently

--- app/src/main/java/com/example/kidsdrawingapp/DrawingView.kt
+++ app/src/main/java/com/example/kidsdrawingapp/DrawingView.kt
@@ -7,23 +7,26 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Path
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 
-class DrawingView(context: Context,attrs:AttributeSet ): View(context, attrs) {
+class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) {
 
-    private var mDrawPath: CustomPath?=null
-    private var mCanvasBitmap: Bitmap?=null
-    private var mDrawPaint: Paint?=null
-    private var mCanvasPaint: Paint?=null
+    private var mDrawPath: CustomPath? = null
+    private var mCanvasBitmap: Bitmap? = null
+    private var mDrawPaint: Paint? = null
+    private var mCanvasPaint: Paint? = null
     private var mBrushSize: Float = 0.toFloat()
     private var color = Color.BLACK
-    private var canvas: Canvas?=null
+    private var canvas: Canvas? = null
+    // to keep the drawing on the screen
+    private val mPaths = ArrayList<CustomPath>()
 
-    init{
+    init {
         setUpDrawing()
     }
 
-    private fun setUpDrawing(){
+    private fun setUpDrawing() {
         mDrawPaint = Paint()
         mDrawPath = CustomPath(color, mBrushSize)
         mDrawPaint!!.color = color
@@ -34,7 +37,66 @@ class DrawingView(context: Context,attrs:AttributeSet ): View(context, attrs) {
         mBrushSize = 20.toFloat()
     }
 
-    internal inner class CustomPath(var color:Int, var brushThickness:Float): Path(){
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        mCanvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+        canvas = Canvas(mCanvasBitmap!!)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawBitmap(mCanvasBitmap!!, 0f, 0f, mCanvasPaint)
+
+        // to keep the drawing on the screen
+        for(path in mPaths){
+            mDrawPaint!!.strokeWidth = path.brushThickness
+            mDrawPaint!!.color = path.color
+            canvas.drawPath(path, mDrawPaint!!)
+        }
+
+        if (!mDrawPath!!.isEmpty) {
+            mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness
+            mDrawPaint!!.color = mDrawPath!!.color
+            canvas.drawPath(mDrawPath!!, mDrawPaint!!)
+        }
+
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        val touchX = event?.x
+        val touchY = event?.y
+
+        when (event?.action) {
+            MotionEvent.ACTION_DOWN -> {
+                mDrawPath!!.color = color
+                mDrawPath!!.brushThickness = mBrushSize
+
+                mDrawPath!!.reset()
+                if (touchX != null) {
+                    if (touchY != null) {
+                        mDrawPath!!.moveTo(touchX, touchY)
+                    }
+                }
+            }
+            MotionEvent.ACTION_MOVE -> {
+                if (touchX != null) {
+                    if (touchY != null) {
+                        mDrawPath!!.lineTo(touchX, touchY)
+                    }
+                }
+            }
+            MotionEvent.ACTION_UP -> {
+                mDrawPath = CustomPath(color, mBrushSize)
+                mPaths.add(mDrawPath!!)
+            }
+            else -> return false
+        }
+        invalidate()
+
+        return true
+    }
+
+    internal inner class CustomPath(var color: Int, var brushThickness: Float) : Path() {
 
     }
 }
